6b136614e6900c86381270df59db9dff3e49b69a
wongtaisin <wongtaisin1024@gmail.com>
2020-06-15 18:50:16 +0800

    去除人员管理数据中心

--- vitec-new-data-center/src/utils/filters.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import fecha from 'fecha'
-
-Vue.filter('date', (value, formatter = 'YYYY-MM-DD') => {
-  if (value) {
-    return fecha.format(new Date(value), formatter)
-  }
-  return value
-})
